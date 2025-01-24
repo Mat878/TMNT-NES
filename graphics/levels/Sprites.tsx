df1a4fb621741ab45227d8bffac97ac2c3b80561
Mat878 <133916093+Mat878@users.noreply.github.com>
2025-01-24 21:03:54 +0000

    Add files via upload

---
+++ graphics/levels/Sprites.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Sprites" tilewidth="132" tileheight="106" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="7">
+  <image width="48" height="32" source="../enemies/enemy1/0.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="44" source="../enemies/enemy2/0.png"/>
+ </tile>
+ <tile id="10">
+  <image width="50" height="73" source="../enemies/enemy3/idle/0.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../items/pizza.png"/>
+ </tile>
+ <tile id="14">
+  <image width="84" height="32" source="../items/sword.png"/>
+ </tile>
+ <tile id="15">
+  <image width="132" height="106" source="../player/climb_idle/0.png"/>
+ </tile>
+ <tile id="16">
+  <image width="58" height="70" source="../enemies/enemy4/1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="48" height="56" source="../../../../Pictures/a.png"/>
+ </tile>
+ <tile id="18">
+  <image width="48" height="78" source="../../../../Pictures/b.png"/>
+ </tile>
+ <tile id="19">
+  <image width="66" height="80" source="../../../../Pictures/c.png"/>
+ </tile>
+</tileset>
---
+++ graphics/overworld/overworld_sprites.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Leo" tilewidth="48" tileheight="58" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="icon/idle/0.png" width="24" height="36"/>
+ </tile>
+ <tile id="2">
+  <image source="enemy/down/0.png" width="48" height="58"/>
+ </tile>
+ <tile id="4">
+  <image source="enemy/up/1.png" width="48" height="44"/>
+ </tile>
+</tileset>

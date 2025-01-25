f1d01a5ec851a86d6fd317f9d7cb41ca7a7f779d
Mat878 <133916093+Mat878@users.noreply.github.com>
2025-01-25 12:37:19 +0000

    Delete graphics/levels/Tileset1.tsx

--- graphics/levels/Tileset1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Tileset2" tilewidth="32" tileheight="32" tilecount="882" columns="63">
- <image source="../../../../Pictures/background2.png" width="2016" height="448"/>
-</tileset>

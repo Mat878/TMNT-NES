d920e1234bd656e4830ff711c2a4e574583ed5a1
Mat878 <133916093+Mat878@users.noreply.github.com>
2025-01-25 12:37:26 +0000

    Delete graphics/levels/Tileset2.tsx

--- graphics/levels/Tileset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Tileset2" tilewidth="32" tileheight="32" tilecount="658" columns="47">
- <image source="../../../../Pictures/level2.png" width="1504" height="448"/>
-</tileset>
